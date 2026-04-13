cece94de0af9e5b3e98cba5270fee9199dcb74aa
Ryan Miller <ryanmillergm@gmail.com>
2026-04-13 16:10:10 -0600

    Updates basic client with version, helper function and search wikipedia function

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,42 @@
-import 'package:cli/cli.dart' as cli;
+import 'dart:io';
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
 }
+
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into the CLI into a single string
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
