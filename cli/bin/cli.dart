1d26964ff41f8b8d607d8256f637a6d4a0f9be95
Ryan Miller <ryanmillergm@gmail.com>
2026-04-14 23:50:05 -0600

    Updates the search wikipedia to use a live http get request

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -6,7 +8,7 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') { // Changed to 'wikipedia'
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
@@ -15,22 +17,30 @@ void main(List<String> arguments) {
 }
 
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
   // If the user didn't pass in arguments, request an article title.
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    // Await input and provide a default empty string if the input is null.
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync(); // Read input
+
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return; // Exit the function if no valid input
+    }
+
+    articleTitle = inputFromStdin;
+
   } else {
     // Otherwise, join the arguments into the CLI into a single string
     articleTitle = arguments.join(' ');
   }
 
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent); // Print the full article response (raw JSON for now)
 }
 
 
@@ -38,5 +48,20 @@ void printUsage() {
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org', // Wikipedia API domain
+    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
+  );
+  final response = await http.get(url); // Make the HTTP request
+
+  if (response.statusCode == 200) {
+    return response.body; // Return the response body if successful
+  }
+
+  // Return an error message if the request failed
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
 
